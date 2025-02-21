c4b25423d0598473cb7063efe7525148302c3270
michelenatale <104991886+michelenatale@users.noreply.github.com>
2025-02-22 00:43:44 +0100

    Update

---
+++ Converts/BaseConverter/TestBaseConverter/BaseConverter/BaseConverterBigInteger.cs
@@ -0,0 +1,52 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace TestBaseConverter.BaseConverter; 
+
+public class BaseConverterBigInteger
+{
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public static byte[] Converter(
+    ReadOnlySpan<byte> bytes, int startbase, int targetbase)
+  {
+    var base10 = startbase == 10 ? bytes : FromBaseX(bytes, startbase);
+    if (targetbase == 10) return base10.ToArray();
+    return ToBaseX(base10, targetbase);
+  }
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public static byte[] FromBaseX(ReadOnlySpan<byte> bytes, int basex)
+  {
+    //From Base X to Base 10
+    if (basex == 10) return [.. bytes.ToArray()];
+
+    var length = bytes.Length;
+    var bi = BigInteger.Zero;
+    for (var i = 0; i < length; i++)
+      bi += bytes[^(1 + i)] * BigInteger.Pow(basex, i);
+
+    return [.. bi.ToString().Select(x => (byte)(x - 48))];
+  }
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public static byte[] ToBaseX(ReadOnlySpan<byte> bytes_base10, int basex) =>
+    ToBaseX(BigInteger.Parse(string.Join("", bytes_base10.ToArray())), basex);
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public static byte[] ToBaseX(BigInteger base10, int basex)
+  {
+    //From Base 10 to Base X
+    if (basex == 10) return [.. base10.ToString().Select(x => (byte)(x - 48))];
+
+    var tmp = new Stack<byte>();
+    while (base10 != 0)
+    {
+      tmp.Push((byte)(base10 % basex));
+      base10 /= basex;
+    }
+
+    var result = tmp.ToArray(); 
+    return result;
+  }
+}
