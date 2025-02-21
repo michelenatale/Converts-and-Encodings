7d12e8a1b227c7b59a02af2ccfe817c9b645a0fa
michelenatale <104991886+michelenatale@users.noreply.github.com>
2025-02-22 00:09:27 +0100

    Update

--- Converters/BaseConverter/TestBaseConverter/BaseConverter/BaseConverterBigInteger.cs
+++ Converters/BaseConverter/TestBaseConverter/BaseConverter/BaseConverterBigInteger.cs
@@ -36,7 +36,7 @@ public class BaseConverterBigInteger
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
   public static byte[] ToBaseX(BigInteger base10, int basex)
   {
-    //From Base 10 to Base X (Reverse !)
+    //From Base 10 to Base X
     if (basex == 10) return [.. base10.ToString().Select(x => (byte)(x - 48))];
 
     var tmp = new Stack<byte>();
